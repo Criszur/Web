e1c8d345f75069116af672bd56a4adef05d789b2
Cristina <>
2021-06-04 22:25:50 +0000

    move files around

--- Phyton/diceroll.py
+++
@@ -1,9 +0,0 @@
-import random
-count = 0
-dicerolls = 0
-def dicerolls ():
-    return random.randint (1,6)
-user = int(input("How many games would you like to roll ?"))
-for i in range (user):
-    guesses = dicerolls()
-    print("The guess is", guesses)
---
+++ Root/Lab11/Lab11.js
@@ -0,0 +1,26 @@
+//Aritmetic and Assignament Operators   
+//var number1= 7;
+//var number2= 4;
+//var number1 = number1 + number2;
+//number1 += number2;
+//number1--;
+
+//document.write (number1);
+
+
+//condition
+//first create a var then condition 
+//var food ="Meat"
+//if(food=="Apple"){
+//alert("We ate some fruit and specificly an Apple");
+//}
+//else if (food=="Meat"){
+//  alert("we ate more than Meat");
+//}
+
+var 1=1;
+
+while(1<5){
+  document.write ("A statement has run");
+  1++;
+}
